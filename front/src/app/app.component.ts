6f59e095281b81abdd73fb37ec8faa7d17d6b246
MrBamboozle <filip.petrina92@gmail.com>
2024-01-06 13:28:43 +0100

    header added, sign in button/profile dropdown menu added, bootstrap styling + spacer variables overrrides

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, NgbDropdownModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   public isSidebarHidden: boolean = false;
-  title = 'Project Mayhem';
+  public isSignedIn: boolean = false;
+  public title: string = 'Project Mayhem';
 
   public toggleSidebar(): void {
     this.isSidebarHidden = !this.isSidebarHidden;
+  }
+
+  public signIn(): void {
+    this.isSignedIn = true;
+  }
+
+  public signOut(): void {
+    this.isSignedIn = false;
   }
 }
