f5e56477c5e1c8d4117524aa832e09fb4e82ad3a
MrBamboozle <filip.petrina92@gmail.com>
2024-01-15 11:35:36 +0100

    profile page split into 3 section with tab navigation

---
+++ front/src/app/pages/profile/components/change-avatar/change-avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-avatar',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-avatar.component.html',
+  styleUrl: './change-avatar.component.scss'
+})
+export class ChangeAvatarComponent {
+
+}
