f17579261a9b89f4257da9b53a0c70eabb62ceb8
MrBamboozle <filip.petrina92@gmail.com>
2024-01-15 10:56:17 +0100

    confirm modal added

--- front/src/app/shared/services/modal-helper.service.ts
+++ front/src/app/shared/services/modal-helper.service.ts
@@ -1,5 +1,6 @@
 import { Component, Injectable, Type } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { ConfirmDialogModalComponent } from '../modals/confirm-dialog.modal/confirm-dialog.modal.component';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,32 @@ export class ModalHelperService {
     });
 
     return modalRef;
+  }
+
+  openCofirmModal(
+    title: string,
+    message: string,
+    confirmFn: () => void,
+    cancelFn: () => void = () => {},
+  ) {
+    const modalRef = this.modalService.open(ConfirmDialogModalComponent);
+    modalRef.componentInstance.title = title; // Customize title
+    modalRef.componentInstance.message = message; // Customize message
+
+    modalRef.shown.subscribe(() => {
+      modalRef.update({modalDialogClass: 'show-modal'})
+    });
+
+    modalRef.result.then((result) => {
+      if (result === 'confirm') {
+        // Logic to execute on confirmation
+        confirmFn();
+      }
+    }, (reason) => {
+      if (reason === 'cancel') {
+        // Logic to execute on dismissal
+        cancelFn();
+      }
+    });
   }
 }
