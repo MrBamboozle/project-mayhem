ff1d199345d38c29090f23444e2b7d5561f117c3
MrBamboozle <filip.petrina92@gmail.com>
2024-01-16 14:21:35 +0100

    avatar upload with cropping and recursive compression implemented

--- front/src/app/shared/components/change-avatar/change-avatar.component.ts
+++ front/src/app/shared/components/change-avatar/change-avatar.component.ts
@@ -4,11 +4,13 @@ import { config } from '@app/core/app-config';
 import { AvatarsService } from '@app/services/avatars.service';
 import { UsersService } from '@app/services/users.service';
 import { Avatar, User } from '@app/shared/models/user';
+import { ImageCroppedEvent, ImageCropperModule } from 'ngx-image-cropper';
+import Compressor from 'compressorjs';
 
 @Component({
   selector: 'app-change-avatar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ImageCropperModule],
   templateUrl: './change-avatar.component.html',
   styleUrl: './change-avatar.component.scss'
 })
@@ -25,6 +27,7 @@ export class ChangeAvatarComponent {
   constructor(
     private readonly avatarService: AvatarsService,
     private readonly userService: UsersService,
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit(): void {
@@ -56,5 +59,52 @@ export class ChangeAvatarComponent {
         this.defaultAvatars = data;
       }
     )
+  }
+
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  compressedImage: any = '';
+  imageCroppedEvent!: ImageCroppedEvent;
+
+  onFileChange(event: any): void {
+    this.imageChangedEvent = event;
+  }
+
+  imageCropped(event: ImageCroppedEvent) {
+    console.log(event)
+    this.imageCroppedEvent = event;
+  }
+
+  onCropFinished() {
+    // The user has finished cropping the image
+    this.croppedImage = this.imageCroppedEvent.blob;
+    this.compressImage(this.croppedImage, 0.8, 102400*3); // Initial quality and target size (100KB multiplied by a factor)
+  }
+
+  compressImage(imageSrc: any, quality: number, targetSize: number, minQuality = 0.4) {
+    new Compressor(imageSrc, {
+      quality: quality,
+      success: (compressedImage) => {
+        if (compressedImage.size > targetSize && quality > minQuality) {
+          console.warn('bigger', compressedImage.size, targetSize, quality, compressedImage);
+          const newQuality = quality - 0.1 > minQuality ? quality - 0.1 : minQuality;
+          this.compressImage(imageSrc, newQuality, targetSize, minQuality);
+        } else {
+          // Handle the compressed image
+          console.log('Final Image Size:', compressedImage.size);
+          this.uploadImage(compressedImage); // Call your upload function
+        }
+      },
+      error: (err) => {
+        console.error(err.message);
+      }
+    });
+  }
+
+  uploadImage(image: Blob) {
+    console.log('great success!!!', URL.createObjectURL(image))
+    this.compressedImage = URL.createObjectURL(image);
+    this.cdr.detectChanges();
+    // Code to upload the image to your server
   }
 }
